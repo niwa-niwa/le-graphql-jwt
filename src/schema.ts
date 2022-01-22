68f7e0c4b56b9f86b3f578dd9c40a4f992e09074
niwa-niwa <urban.hang274@gmail.com>
2022-01-22 18:49:16 +0900

    implemented gql scheme

--- src/schema.ts
+++ src/schema.ts
@@ -1,3 +1,78 @@
-import {gql} from 'apollo-server';
+const { gql } = require("apollo-server");
 
-const typeDefs = gql``
+export const typeDefs = gql`
+  type Query {
+    posts(query: String): [Post!]!
+    users(query: String): [User!]!
+  }
+
+  type Mutation {
+    createPost(data: CreatePostInput!): Post!
+    deletePost(id: ID!): Post!
+    updatePost(id: ID!, data: UpdatePostInput!): Post!
+
+    createUser(data: CreateUserInput!): AuthPayload!
+    login(data: LoginUserInput!): AuthPayload!
+  }
+
+  type Subscription {
+    post: PostSubscriptionPayload!
+  }
+
+  enum MutationType {
+    CREATED
+    UPDATED
+    DELETED
+  }
+
+  type PostSubscriptionPayload {
+    mutation: MutationType!
+    data: Post!
+  }
+
+  input CreatePostInput {
+    title: String!
+    author: String!
+    postedUser: ID!
+  }
+
+  input UpdatePostInput {
+    title: String
+    author: String!
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
+  input CreateUserInput {
+    name: String!
+    email: String!
+    password: String!
+  }
+
+  input LoginUserInput {
+    email: String!
+    password: String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String
+    password: String!
+    posts: [Post!]!
+    updatedAt: String!
+    createdAt: String!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    author: String!
+    postedUser: User!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
